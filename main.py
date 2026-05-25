4807b01f708456b262970c3c65daa3a8865ab69b
Evan <evanfost@andrew.cmu.edu>
2026-05-25 10:15:54 -0400

    💩 write basic server implementation

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from Chihuahua!")
+from fasthtml.common import *
 
+import sys
+
+is_debug = "--debug" in sys.argv
+
+app, rt = fast_app(
+    live=is_debug, # enable live reloading
+    debug=is_debug, # enable extended tracebacks on error
+)
+
+@rt("/")
+def get():
+    return Titled("Chihuahua", P("Let's do this!"))
 
 if __name__ == "__main__":
-    main()
+    serve()
